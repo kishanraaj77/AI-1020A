b69e1e2abf170fe38a6fae0b9a168bd87fd38ad4
kishanraaj77 <9276622+kishanraaj77@users.noreply.github.com>
2021-03-24 02:32:37 +0800

    Update config.py

--- herocard/config.py
+++ herocard/config.py
@@ -4,5 +4,5 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "7c6e905c-f589-4037-ab6d-9a2e051b805b")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "4d4a4c6b-b0c7-45eb-b835-587215ca7429")
+    APP_ID = os.environ.get("MicrosoftAppId", "053b477c-6f0b-4629-a12d-6bef846605ac")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "2da80739-ab3a-4fb1-8fa1-c4df64b087f2")
