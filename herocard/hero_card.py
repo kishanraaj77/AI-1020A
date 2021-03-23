18d03549831ffb249dd265afc9cc13a7f97f1102
kishanraaj77 <9276622+kishanraaj77@users.noreply.github.com>
2021-03-24 02:27:47 +0800

    Update hero_card.py

--- herocard/hero_card.py
+++ herocard/hero_card.py
@@ -1,20 +1,18 @@
-from botbuilder.core import TurnContext,ActivityHandler,MessageFactory,CardFactory
-from botbuilder.schema import Attachment,MediaUrl,HeroCard,CardImage,CardAction,ActionTypes
+from botbuilder.core import ActivityHandler, TurnContext
+from botbuilder.schema import ChannelAccount
 
 
-class SampleAnimationCard(ActivityHandler):
-   def __init__(self):
-      pass
+class MyBot(ActivityHandler):
+    # See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
 
-   async def on_message_activity(self,turn_context:TurnContext):
-      cardAtt = self.create_hero_card()
-      msg_activity = MessageFactory.attachment(cardAtt)
-      await turn_context.send_activity(msg_activity)
-      
+    async def on_message_activity(self, turn_context: TurnContext):
+        await turn_context.send_activity(f"You said '{ turn_context.activity.text }'")
 
-   def create_hero_card(self) -> Attachment:
-      herocard = HeroCard(title="Sample of Hero Card in Bot using Python",
-      images=[CardImage(url="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Seattle_monorail01_2008-02-25.jpg/1024px-Seattle_monorail01_2008-02-25.jpg")],
-      buttons=[CardAction(type=ActionTypes.open_url,title="Open Url",value="https://dev.botframework.com/")])
-      return CardFactory.hero_card(herocard)
-   
+    async def on_members_added_activity(
+        self,
+        members_added: ChannelAccount,
+        turn_context: TurnContext
+    ):
+        for member_added in members_added:
+            if member_added.id != turn_context.activity.recipient.id:
+                await turn_context.send_activity("Hello and welcome!")
